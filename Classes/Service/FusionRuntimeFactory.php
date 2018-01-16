3bc44ff71c0e7538461ddf4f0559f547e5658a10
Martin Ficzel <ficzel@sitegeist.de>
2018-01-16 15:59:31 +0100

    TASK: Update fusion renderer to configure the prototype implementation overrides locally

--- Classes/Service/FusionRuntimeFactory.php
+++ Classes/Service/FusionRuntimeFactory.php
@@ -3,6 +3,8 @@ namespace Sitegeist\Typo3\Fusion\Renderer\Service;
 
 use Sitegeist\Fusion\Standalone\Core\Runtime;
 use Neos\Utility\Files;
+use Neos\Utility\Arrays;
+
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class FusionRuntimeFactory
@@ -43,11 +45,33 @@ class FusionRuntimeFactory
      */
     public function getRuntime($astFile) {
         if (array_key_exists($astFile, $this->runtimes) === false) {
+            //
+            // read fusion ast
+            //
             $fusionAstFileName = GeneralUtility::getFileAbsFileName($astFile);
-            $defaultContextConfiguration = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['typo3_fusion_renderer']['defaultContextConfiguration'];
             $ast = json_decode(file_get_contents($fusionAstFileName), true);
+
+            //
+            // prepare ast with local implementations from conf vars
+            //
+            $fusionObjectImplementations = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['typo3_fusion_renderer']['fusionObjectImplementations'] ?: [];
+            foreach ($fusionObjectImplementations as $fusionObjectName => $fusionObjectImplementation) {
+                if (Arrays::getValueByPath($ast, ['__prototypes', $fusionObjectName])) {
+                    $ast = Arrays::setValueByPath($ast, ['__prototypes', $fusionObjectName, '__meta', 'class'], $fusionObjectImplementation);
+                }
+            }
+
+            //
+            // create eel configuration from conf vars
+            //
             Files::createDirectoryRecursively(PATH_site . 'typo3temp/typo3_fusion_renderer');
-            $runtime = new Runtime($ast,PATH_site . 'typo3temp/typo3_fusion_renderer/eel', $defaultContextConfiguration);
+            $eelCacheDirectory = PATH_site . 'typo3temp/typo3_fusion_renderer/eel';
+            $eelHelperConfiguration = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['typo3_fusion_renderer']['eelHelperImplementations'] ?: [];
+
+            //
+            // create runtime
+            //
+            $runtime = new Runtime($ast, $eelCacheDirectory, $eelHelperConfiguration);
             $this->runtimes[$astFile] = $runtime;
         }
         return $this->runtimes[$astFile];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,37 @@
 <?php
+
 use Sitegeist\Eel\Standalone\Helper as EelHelper;
+use Sitegeist\Fusion\Standalone\FusionObjects as FusionObjects;
 
 //
 // register default fusion context configurations
 // the classes defined for each key will be instantiated
 // and added to the default context of the fusion runtime
 //
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['typo3_fusion_renderer']['defaultContextConfiguration'] = [
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['typo3_fusion_renderer']['eelHelperImplementations'] = [
     'Array' => EelHelper\ArrayHelper::class,
     'Date' => EelHelper\DateHelper::class,
     'Json' => EelHelper\JsonHelper::class,
     'Math' => EelHelper\MathHelper::class,
     'String' => EelHelper\StringHelper::class,
     'Type' => EelHelper\TypeHelper::class
+];
+
+//
+// register default fusion-object implementations
+// the classes defined for each key will be instantiated
+// and added to the default context of the fusion runtime
+//
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['typo3_fusion_renderer']['fusionObjectImplementations'] = [
+    'Neos.Fusion:Array' => FusionObjects\ArrayImplementation::class,
+    'Neos.Fusion:Value' => FusionObjects\ValueImplementation::class,
+    'Neos.Fusion:Component' => FusionObjects\ComponentImplementation::class,
+    'Neos.Fusion:Collection' => FusionObjects\CollectionImplementation::class,
+    'Neos.Fusion:Augmenter' => FusionObjects\AugmenterImplementation::class,
+    'Neos.Fusion:Attributes' => FusionObjects\AttributesImplementation::class,
+    'Neos.Fusion:Tag' => FusionObjects\TagImplementation::class,
+    'Neos.Fusion:RawArray' => FusionObjects\RawArrayImplementation::class,
+    'Neos.Fusion:RawCollection' =>  FusionObjects\RawCollectionImplementation::class,
+    'PackageFactory.AtomicFusion:Component' => FusionObjects\ComponentImplementation::class,
+    'PackageFactory.AtomicFusion:Augmenter' => FusionObjects\AugmenterImplementation::class
 ];
